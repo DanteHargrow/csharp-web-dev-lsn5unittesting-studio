e517dae80da7ac19c854e4e02a7ce81a09364d4f
dantehargrow <dantehargrow@gmail.com>
2020-10-15 19:40:08 -0500

    Wrote 12 test

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -9,6 +9,62 @@ namespace BalancedBracketsTests
         // TODO: Add tests to this file.
 
         [TestMethod]
+        public void NoBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Test Message"));
+        }
+        [TestMethod]
+        public void EmptyStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+        [TestMethod]
+        public void MissingBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("Test]"));
+        }
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+        [TestMethod]
+        public void isNotBalancedReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("Launch]Code["));
+        }
+        [TestMethod]
+        public void OneBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+        [TestMethod]
+        public void IsNotNull()
+        {
+            Assert.IsNotNull(BalancedBrackets.HasBalancedBrackets("Test"));
+        }
+        [TestMethod]
+        public void OnlyBracketsButBackwardsReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+        [TestMethod]
+        public void WordOutsideOfBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Launch[Code]"));
+        }
+        [TestMethod]
+        public void NoWordInBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]Test"));
+        }
+        [TestMethod]
+        public void MisNest()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[[Tes[]t]"));
+        }
+
+        [TestMethod]
         public void EmptyTest()
         {
             Assert.AreEqual(true, true);
